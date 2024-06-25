74cd7ed5964ae42227d80af44dd1393206b53004
Alexey Gorbenko <a.v.gor@mail.ru>
2024-06-25 15:01:49 +0300

    feat: save search input in LocalStorage

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -1,45 +1,35 @@
 import { ChangeEvent, Component, ReactNode } from "react";
 import './search-form.css';
 
-export default class SearchForm extends Component<unknown, { query: string }> {
+interface SearchFormState {
+  query: string
+}
+
+export default class SearchForm extends Component <Component, SearchFormState> {
   constructor(props: Component) {
     super(props);
-    this.state = {query: ''}
+    this.state = {
+      query: ''
+    }
   }
 
-  saveQuery(string: string | undefined | null) {
-    if(typeof string === 'string') {
-      this.setState({
-        query: string
-      });
-    };
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    this.setState({
+      query: event.target.value
+    });
+    console.log(event.target.value);
   }
 
-  // componentDidMount(): void {
-  //   const queryString = localStorage.getItem('ag-pokemon-query');
-  //   this.saveQuery(queryString);
-  // }
-
-  componentWillUnmount(): void {
-    localStorage.setItem('ag-pokemon-query', this.state.query);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
-    if (!event.target) return;
-    const queryString: string =  event.target.value;
-    if(typeof queryString === 'string') {
-      this.setState({
-        query: queryString
-      });
-    };
+  handleSubmit = () => {
+    localStorage.setItem('ag-pokemon-database', this.state.query);
   }
 
   render(): ReactNode {
     return (
-      <form className="search-form">
+      <form className="search-form" onSubmit={this.handleSubmit}>
         <label htmlFor="search-form" className="search-form__label">Search the Pokemon: </label>
-        <input type="search" name="q" id="search-form" className="search-form__input" value={this.state.query}/>
-        <button className="search-form__button">Search</button>
+        <input type="search" id="search-form" className="search-form__input" onChange={this.handleInputChange}/>
+        <button type="submit" className="search-form__button">Search</button>
       </form>
     );
   }
