c29d9cf0700d20c80eb53c95e422ba052c51cd5c
Alexey Gorbenko <a.v.gor@mail.ru>
2024-06-25 15:51:55 +0300

    feat: load search from LocalStorage

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Component, ReactNode } from "react";
+import { ChangeEvent, Component, FormEvent, ReactNode } from "react";
 import './search-form.css';
 
 interface SearchFormState {
@@ -20,15 +20,21 @@ export default class SearchForm extends Component <Component, SearchFormState> {
     console.log(event.target.value);
   }
 
-  handleSubmit = () => {
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     localStorage.setItem('ag-pokemon-database', this.state.query);
+  }
+
+  getQueryFromLS = (): string => {
+    const result = localStorage.getItem('ag-pokemon-database');
+    return result ?? '';
   }
 
   render(): ReactNode {
     return (
       <form className="search-form" onSubmit={this.handleSubmit}>
         <label htmlFor="search-form" className="search-form__label">Search the Pokemon: </label>
-        <input type="search" id="search-form" className="search-form__input" onChange={this.handleInputChange}/>
+        <input type="search" id="search-form" className="search-form__input" onChange={this.handleInputChange} defaultValue={this.getQueryFromLS()}/>
         <button type="submit" className="search-form__button">Search</button>
       </form>
     );
